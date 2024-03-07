ac10abdee13dca3ba8d66bf3c80017a9b5dd16af
Dominik Mach <xxiamczechxx@Dominiks-MacBook-Pro.local>
2024-03-07 01:29:09 +0100

    Broken after adding a turbo

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
